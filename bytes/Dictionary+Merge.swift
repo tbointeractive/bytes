92cc183b17dfc86d541b2d1e60697f68335df2f5
Thorsten Stark <stark@tbointeractive.com>
2016-12-21 13:57:58 +0100

    added second method that returns new dictionary with merged content

--- bytes/Dictionary+Merge.swift
+++ bytes/Dictionary+Merge.swift
@@ -12,11 +12,25 @@ extension Dictionary {
     
     /// Adds all keys and values from dictionary.
     /// - note: All values in self with keys being in both dictionaries will be replaced with the values in dictionary.
+    /// - note: The dictionary retains (not copies) the values from the passed dictionary.
     ///
     /// - Parameter dictionary: The dictionary which keys and values to be added.
     public mutating func merge(_ dictionary:Dictionary) {
         for (key, value) in dictionary{
             self[key] = value
         }
+    }
+    
+    /// Returns a new dictionary with the keys and values form both dictionaries.
+    /// - note: All values in self with keys being in both dictionaries will be replaced with the values in dictionary.
+    /// - note: The new dictionary retains (not copies) the values from both original dictionaries.
+    ///
+    /// - Parameter dictionary: The dictionary which keys and values to be added.
+    /// - Returns: A new dictionary
+    public func merged(_ dictionary: Dictionary) -> Dictionary {
+        var newDictionary = Dictionary()
+        newDictionary.merge(self)
+        newDictionary.merge(dictionary)
+        return newDictionary
     }
 }
--- bytesTests/Dictionary+MergeSpecs.swift
+++ bytesTests/Dictionary+MergeSpecs.swift
@@ -14,7 +14,7 @@ import Nimble
 class DictionaryMergeSpecs: QuickSpec {
     override func spec() {
         describe("merge") {
-            it("should add second dictionary when no keys overlap") {
+            it("should add every key-value pair of the second dictionary when no keys overlap") {
                 var dictA = ["a":"b"]
                 let dictB = ["c":"d"]
                 dictA.merge(dictB)
@@ -22,7 +22,7 @@ class DictionaryMergeSpecs: QuickSpec {
                 expect(dictA["a"]).to(equal("b"))
                 expect(dictA["c"]).to(equal("d"))
             }
-            it("should override keys when already exited") {
+            it("should override every key-value pair when the key already exists") {
                 var dictA = ["a":"b"]
                 let dictB = ["a":"e"]
                 dictA.merge(dictB)
@@ -48,16 +48,14 @@ class DictionaryMergeSpecs: QuickSpec {
                 let dictB:[AnyHashable:Any] = ["c":3]
                 dictA.merge(dictB)
                 expect(dictA.count) == 2
-                let value = dictA["c"] as! Int
-                expect(value).to(equal(3))
+                expect(dictA["c"] as? Int).to(equal(3))
             }
             it("should work with strings and numbers as keys") {
                 var dictA:[AnyHashable:Any] = ["a":"b"]
                 let dictB:[AnyHashable:Any] = [3:"c"]
                 dictA.merge(dictB)
                 expect(dictA.count) == 2
-                let value = dictA[3] as! String
-                expect(value).to(equal("c"))
+                expect(dictA[3] as? String).to(equal("c"))
             }
             it("should work with different types for keys ans values") {
                 var dictA:[AnyHashable:Any] = ["a":"b", 2: [1, 2], 3:true]
@@ -68,7 +66,60 @@ class DictionaryMergeSpecs: QuickSpec {
                 expect(dictA["a"] as? Double).to(equal(42.1))
             }
         }
-        
+        describe("merged") {
+            it("should add all key-value pairs from both dictionaries to a new one") {
+                let dictA = ["a":"b"]
+                let dictB = ["c":"d"]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 2
+                expect(dictC["a"]).to(equal("b"))
+                expect(dictC["c"]).to(equal("d"))
+            }
+            it("should override every key-value pair when the key already exists in the first dictionary") {
+                let dictA = ["a":"b"]
+                let dictB = ["a":"e"]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 1
+                expect(dictC["a"]).to(equal("e"))
+            }
+            it("should be first dictionary if second dictionary is empty") {
+                let dictA = ["a":"b"]
+                let dictB: [String:String] = [:]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 1
+                expect(dictC).to(equal(dictA))
+            }
+            it("should be second dictionary if first dictionary is empty") {
+                let dictA: [String:String] = [:]
+                let dictB = ["c":"d"]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 1
+                expect(dictC).to(equal(dictB))
+            }
+            it("should work with strings and numbers as values") {
+                let dictA:[AnyHashable:Any] = ["a":"b"]
+                let dictB:[AnyHashable:Any] = ["c":3]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 2
+                expect(dictC["c"] as? Int).to(equal(3))
+            }
+            it("should work with strings and numbers as keys") {
+                let dictA:[AnyHashable:Any] = ["a":"b"]
+                let dictB:[AnyHashable:Any] = [3:"c"]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 2
+                expect(dictC[3] as? String).to(equal("c"))
+            }
+            it("should work with different types for keys ans values") {
+                let dictA:[AnyHashable:Any] = ["a":"b", 2: [1, 2], 3:true]
+                let dictB:[AnyHashable:Any] = [3:"c", "a":42.1, "key":["dict":"value"]]
+                let dictC = dictA.merged(dictB)
+                expect(dictC.count) == 4
+                expect(dictC[3] as? String).to(equal("c"))
+                expect(dictC["a"] as? Double).to(equal(42.1))
+            }
+        }
+
     }
 }
 
