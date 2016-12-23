4e5528f452340bf14ee2e236df658d1073985036
Thorsten Stark <stark@tbointeractive.com>
2016-12-23 16:01:54 +0100

    added extensions for String, Int and Bool including tests

---
+++ bytesTests/Int+RandomSpec.swift
@@ -0,0 +1,37 @@
+//
+//  Int+RandomSpec.swift
+//  bytes
+//
+//  Created by Thorsten Stark on 23.12.16.
+//  Copyright © 2016 TBO INTERACTIVE GmbH & Co. KG. All rights reserved.
+//
+
+@testable import bytes
+
+import Quick
+import Nimble
+
+class IntRandomSpec: QuickSpec {
+    override func spec() {
+        describe("randomInRange") {
+            it ("should return an integer value inside the given range") {
+                let range:Range = 5 ..< 10
+                let randomInt = Int.random(in: range)
+                expect(range.contains(randomInt)).to(beTrue())
+            }
+            it ("should return allwas the same value when range has length of one") {
+                let range:Range = 2 ..< 3
+                for _ in 1 ..< 100 {
+                    let randomInt = Int.random(in: range)
+                    expect(randomInt) == 2
+                }
+            }
+            it ("should return 0 if the range is empty") {
+                let range:Range = 1..<1
+                let randomInt = Int.random(in: range)
+                expect(range.isEmpty).to(beTrue())
+                expect(randomInt) == 0
+            }
+        }
+    }
+}
