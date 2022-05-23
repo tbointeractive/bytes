1f2624e6a129ff502c89655469d03604384e6eed
Thorsten Stark <mail@thorsten-stark.de>
2022-05-23 10:43:28 +0200

    add path to target in package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "bytes",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         // .testTarget(
         //     name: "bytesTests",
         //     dependencies: ["bytes"]),
