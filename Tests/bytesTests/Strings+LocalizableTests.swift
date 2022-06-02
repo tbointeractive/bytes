4776b7863649ef22d4a80604b7da78a11a39f30e
Thorsten Stark <mail@thorsten-stark.de>
2022-06-02 10:57:33 +0200

    improve localized() test names

--- Tests/bytesTests/Strings+LocalizableTests.swift
+++ Tests/bytesTests/Strings+LocalizableTests.swift
@@ -9,13 +9,23 @@ import XCTest
 
 class Strings_LocalizableTests: XCTestCase {
 
-    func testSimpleLocalizedWithBundle() throws {
+    func testLocalizedWithBundle() throws {
         XCTAssertEqual("test.key".localized(bundle: .module), "it works!")
     }
     
-    func testSimpleLocalizedWithCorrectKey() throws {
+    func testLocalizedWithBundleAndWrongKey() throws {
+        XCTAssertEqual("test.unknown.key".localized(bundle: .module), "test.unknown.key")
+    }
+    
+    func testLocalizedWithTableAndBundle() throws {
         XCTAssertEqual("more.key".localized(tableName: "More", bundle: .module), "This works, too.")
     }
-
-
+    
+    func testLocalizedWithWrongTableAndBundle() throws {
+        XCTAssertEqual("more.key".localized(tableName: "Unknwon", bundle: .module), "more.key")
+    }
+    
+    func testLocalizedWithTableAndBundleAndWrongKey() throws {
+        XCTAssertEqual("more.unknown.key".localized(tableName: "More", bundle: .module), "more.unknown.key")
+    }
 }
